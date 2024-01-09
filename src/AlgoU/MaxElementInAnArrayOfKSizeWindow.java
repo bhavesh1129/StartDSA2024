7f18c53ab0648883dd388592a0f89140e2ddab93
Bhavesh Garg <developerbhaveshgarg@gmail.com>
2024-01-09 20:31:42 +0530

    Add Code

--- src/AlgoU/MaxElementInAnArrayOfKSizeWindow.java
+++ src/AlgoU/MaxElementInAnArrayOfKSizeWindow.java
@@ -4,22 +4,35 @@ import java.util.Arrays;
 
 public class MaxElementInAnArrayOfKSizeWindow {
     public static void main(String[] args) {
-        int[] arr = {10, 12, 9, 8, 10, 15, 1, 3, 2};
-        int k = 3;
+        int[] arr = {1};
+        int k = 1;
         int[] res = maxElmInArrOfKSizeWin(arr, k);
         System.out.println(Arrays.toString(res));
     }
 
     private static int[] maxElmInArrOfKSizeWin(int[] arr, int k) {
-        int[] res = new int[arr.length - k];
+        int[] res = new int[arr.length - k + 1];
         int max = Integer.MIN_VALUE, j = 0;
         for (int i = 0; i < k; i++) {
             max = Math.max(max, arr[i]);
         }
         res[j++] = max;
         for (int i = k; i < arr.length; i++) {
-//            max = max -
+            if (arr[i] > max) {
+                max = arr[i];
+            } else if (arr[i - k] == max) {
+                max = findMax(arr, i - k + 1, i);
+            }
+            res[j++] = max;
         }
         return res;
+    }
+
+    private static int findMax(int[] arr, int i, int j) {
+        int max = Integer.MIN_VALUE;
+        for (int k = i; k <= j; k++) {
+            max = Math.max(max, arr[k]);
+        }
+        return max;
     }
 }
